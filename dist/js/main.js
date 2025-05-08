8940e0bc94f74d58462a93813b61c027b3f8015c
ilyas-draissia <ilyesdraissia@gmail.com>
2025-05-08 18:33:43 +0100

    mid
    figured out basically everything only the transitions

---
+++ dist/js/main.js
@@ -0,0 +1,19 @@
+const menuBtn=document.querySelector(".menu-btn");
+const burger=document.querySelector(".menu-btn__burger");
+const nav=document.querySelector(".nav");
+console.log(window.location.pathname.split('.html/')[1]
+);
+let showMenu=false;
+const toggle = ()=>{
+  burger.classList.toggle("open");
+  nav.classList.toggle("open");
+};
+menuBtn.addEventListener("click",()=>{
+  if(!showMenu){
+    toggle();
+    showMenu=!showMenu;
+  }else{
+    toggle();
+    showMenu;
+  }
+})
